ca41bc930510c79cfb47c1873c79c3193709ba59
popfish <popfish@popfishs-MacBook-Air.local>
2014-04-14 22:55:42 -0700

    Add 2

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
     private static final String SECOND_COMMIT = "Second Commit";
     private static final String THIRD_COMMIT = "Third Commit";
     private static final int first = 1;
-    
+    private static final int second = 2;
+
 }
