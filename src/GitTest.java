c62f6963eedaa9800854900daf1f1a20cb5f5b0c
popfish <popfish@popfishs-MacBook-Air.local>
2014-04-14 22:55:12 -0700

    Add 1

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,4 +5,6 @@ public class GitTest {
     private static final String FIRST_COMMIT = "First Commit";
     private static final String SECOND_COMMIT = "Second Commit";
     private static final String THIRD_COMMIT = "Third Commit";
+    private static final int first = 1;
+    
 }
